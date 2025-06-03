fd587b762a95bc2279b4210f51a290921db85cc6
Katherine Noemy Vega Arriaza <82059936+KathyArriaza@users.noreply.github.com>
2025-06-03 16:39:35 -0600

    agregando diseño de Temas

---
+++ AppFlashCard/FrmCrudTemas.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace AppFlashCard
+{
+    partial class FrmCrudTemas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbTemas = new Label();
+            lblMateria = new Label();
+            lblTema = new Label();
+            cmbMaterias = new ComboBox();
+            txtTema = new TextBox();
+            btnGuardar = new Button();
+            btnSalir = new Button();
+            dgvTemas = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvTemas).BeginInit();
+            SuspendLayout();
+            // 
+            // lbTemas
+            // 
+            lbTemas.AutoSize = true;
+            lbTemas.ForeColor = SystemColors.ControlLightLight;
+            lbTemas.Location = new Point(68, 53);
+            lbTemas.Name = "lbTemas";
+            lbTemas.Size = new Size(85, 15);
+            lbTemas.TabIndex = 0;
+            lbTemas.Text = "Agregar Temas";
+            // 
+            // lblMateria
+            // 
+            lblMateria.AutoSize = true;
+            lblMateria.ForeColor = SystemColors.ControlLightLight;
+            lblMateria.Location = new Point(12, 115);
+            lblMateria.Name = "lblMateria";
+            lblMateria.Size = new Size(50, 15);
+            lblMateria.TabIndex = 1;
+            lblMateria.Text = "Materia:";
+            // 
+            // lblTema
+            // 
+            lblTema.AutoSize = true;
+            lblTema.ForeColor = SystemColors.ControlLightLight;
+            lblTema.Location = new Point(17, 161);
+            lblTema.Name = "lblTema";
+            lblTema.Size = new Size(38, 15);
+            lblTema.TabIndex = 2;
+            lblTema.Text = "Tema:";
+            // 
+            // cmbMaterias
+            // 
+            cmbMaterias.FormattingEnabled = true;
+            cmbMaterias.Location = new Point(68, 112);
+            cmbMaterias.Name = "cmbMaterias";
+            cmbMaterias.Size = new Size(141, 23);
+            cmbMaterias.TabIndex = 3;
+            // 
+            // txtTema
+            // 
+            txtTema.Location = new Point(61, 158);
+            txtTema.Name = "txtTema";
+            txtTema.Size = new Size(148, 23);
+            txtTema.TabIndex = 4;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(12, 230);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(75, 23);
+            btnGuardar.TabIndex = 5;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(134, 230);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 6;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // dgvTemas
+            // 
+            dgvTemas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvTemas.Location = new Point(276, 31);
+            dgvTemas.Name = "dgvTemas";
+            dgvTemas.RowTemplate.Height = 25;
+            dgvTemas.Size = new Size(240, 224);
+            dgvTemas.TabIndex = 7;
+            dgvTemas.CellContentClick += dgvTemas_CellContentClick;
+            // 
+            // FrmCrudTemas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(526, 274);
+            Controls.Add(dgvTemas);
+            Controls.Add(btnSalir);
+            Controls.Add(btnGuardar);
+            Controls.Add(txtTema);
+            Controls.Add(cmbMaterias);
+            Controls.Add(lblTema);
+            Controls.Add(lblMateria);
+            Controls.Add(lbTemas);
+            Name = "FrmCrudTemas";
+            Text = "FrmCrudTemas";
+            Load += FrmCrudTemas_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvTemas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbTemas;
+        private Label lblMateria;
+        private Label lblTema;
+        private ComboBox cmbMaterias;
+        private TextBox txtTema;
+        private Button btnGuardar;
+        private Button btnSalir;
+        private DataGridView dgvTemas;
+    }
+}
---
+++ AppFlashCard/FrmCrudTemas.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppFlashCard
+{
+    public partial class FrmCrudTemas : Form
+    {
+        // Fuentes Raleway
+        PrivateFontCollection fuentesPersonalizadas = new PrivateFontCollection();
+        Font ralewayRegular;
+        Font ralewaySemiBold;
+        public FrmCrudTemas()
+        {
+            InitializeComponent();
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+        }
+
+        private void FrmCrudTemas_Load(object sender, EventArgs e)
+        {
+
+            // 2) Asignar las fuentes a los controles
+            txtTema.Font = ralewayRegular;
+            btnGuardar.Font = ralewaySemiBold;
+            btnSalir.Font = ralewaySemiBold;
+            lblMateria.Font = ralewaySemiBold;
+            lbTemas.Font = ralewaySemiBold;
+            lblTema.Font = ralewaySemiBold;
+
+            // 3) Configurar estilo del formulario
+            this.BackColor = ColorTranslator.FromHtml("#2D3250");
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+            // 4) Bordes redondeados y hover en botones
+            btnGuardar.Region = new Region(CrearRegionRedondeada(btnGuardar.ClientRectangle, 7));
+            btnSalir.Region = new Region(CrearRegionRedondeada(btnSalir.ClientRectangle, 7));
+            AgregarHover(btnGuardar, ColorTranslator.FromHtml("#B76F9D"));
+            AgregarHover(btnSalir, ColorTranslator.FromHtml("#B76F9D"));
+            btnSalir.ForeColor = Color.White;
+        }
+
+        #region == Insertar / Editar Materia ==
+        private void AgregarHover(Button boton, Color originalColor)
+        {
+            boton.BackColor = originalColor;
+            boton.FlatStyle = FlatStyle.Flat;
+            boton.FlatAppearance.BorderSize = 0;
+            boton.ForeColor = Color.White;
+
+            boton.MouseEnter += (s, e) => boton.BackColor = ControlPaint.Light(originalColor);
+            boton.MouseLeave += (s, e) => boton.BackColor = originalColor;
+        }
+
+
+        private GraphicsPath CrearRegionRedondeada(Rectangle rect, int radio)
+        {
+            int diameter = radio;
+            GraphicsPath path = new GraphicsPath();
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+            path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+        private void CargarFuenteRaleway()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            void CargarTTF(string resourceName)
+            {
+                Stream stream = assembly.GetManifestResourceStream(resourceName);
+                if (stream == null)
+                {
+                    MessageBox.Show($"No se encontró el recurso embebido:\n{resourceName}",
+                                    "Error al cargar fuente",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                    return;
+                }
+
+                byte[] fontData = new byte[stream.Length];
+                stream.Read(fontData, 0, (int)stream.Length);
+
+                IntPtr data = Marshal.AllocCoTaskMem(fontData.Length);
+                Marshal.Copy(fontData, 0, data, fontData.Length);
+                fuentesPersonalizadas.AddMemoryFont(data, fontData.Length);
+            }
+
+
+            CargarTTF("AppFlashCard.Fonts.Raleway-Regular.ttf");
+            CargarTTF("AppFlashCard.Fonts.Raleway-SemiBold.ttf");
+
+            ralewayRegular = new Font(fuentesPersonalizadas.Families[0], 12f, FontStyle.Regular);
+            ralewaySemiBold = new Font(
+                fuentesPersonalizadas.Families.Length > 1
+                    ? fuentesPersonalizadas.Families[1]
+                    : fuentesPersonalizadas.Families[0],
+                12f,
+                FontStyle.Bold
+            );
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dgvTemas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
+#endregion
